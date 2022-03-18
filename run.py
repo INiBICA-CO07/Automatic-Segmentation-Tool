506e1ce772f49143456a0614203ef118291a69c1
Valderas <45321489+Valderas7@users.noreply.github.com>
2022-03-18 12:06:08 +0100

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from waitress import serve
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-
-serve(app, listen='0.0.0.0:443')
-#app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False,  threaded=True,)
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=8080, use_reloader=False,  threaded=True)
-
